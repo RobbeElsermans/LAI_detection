8c20fb4074caa5f7d94d25fe25ba8461ef468c94
Robbe Elsermans <robels@outlook.com>
2023-12-12 19:59:59 +0100

    add image to gui

--- ply_planefit_example.m
+++ ply_planefit_example.m
@@ -1,19 +1,19 @@
 clear
 close all
-pointcloud = pcread('LAI_detection\LC_20231130_152650.ply');
+pointcloud = pcread('High_Coverage/HC_20231130_143832.ply');
 %clean up the pointcloud by setting the distance to an overestimation of the approximate distance to the wall
 % such that only actual outliers are removed
-[model,inlierIndices, outlierIndices] = pcfitplane(pointcloud,1);
+[model,inlierIndices, outlierIndices] = pcfitplane(pointcloud,0.5);
 cleancloud = select(pointcloud, inlierIndices);
 pcshow(cleancloud)
 title("Cleaned dataset, outliners removed")
 
-estimatedLayerThickness = 0.01;
-[model,inlierIndices, outlierIndices] = pcfitplane(cleancloud,estimatedLayerThickness);
-firstlayer = select(cleancloud, inlierIndices);
-figure();pcshow(firstlayer)
-title("Leaves first layer")
-
-secondlayer = select(cleancloud, outlierIndices);
-figure();pcshow(secondlayer)
-title("Leaves second layer")
+% estimatedLayerThickness = 0.01;
+% [model,inlierIndices, outlierIndices] = pcfitplane(cleancloud,estimatedLayerThickness);
+% firstlayer = select(cleancloud, inlierIndices);
+% figure();pcshow(firstlayer)
+% title("Leaves first layer")
+% 
+% secondlayer = select(cleancloud, outlierIndices);
+% figure();pcshow(secondlayer)
+% title("Leaves second layer")
