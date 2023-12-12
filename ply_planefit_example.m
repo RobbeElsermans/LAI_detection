23e7c23820187f670d438c3dd4f3ddf87d5a04b7
ahmadshakleya <shakleyaahmed@gmail.com>
2023-12-12 15:50:33 +0100

    ply_planefit aangepast
    Probleem: plane moet nog rechtgezet worden.

--- ply_planefit_example.m
+++ ply_planefit_example.m
@@ -1,7 +1,7 @@
 clear
 close all
 pointcloud = pcread('LAI_detection\LC_20231130_152650.ply');
-%clean up the pointcloud by setting the distance to an overestimation of the approximate distance to the wall
+% clean up the pointcloud by setting the distance to an overestimation of the approximate distance to the wall
 % such that only actual outliers are removed
 [model,inlierIndices, outlierIndices] = pcfitplane(pointcloud,1);
 cleancloud = select(pointcloud, inlierIndices);
