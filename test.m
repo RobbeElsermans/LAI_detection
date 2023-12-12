2b4b15b2599c861291bf85579ac5b7594c194aff
ThomasKramp <thomas.kramp378@gmail.com>
2023-12-12 14:47:15 +0100

    bhjgnk

--- test.m
+++
@@ -1,5 +0,0 @@
-path = '/home/thomas/Documents/school/semester_3/5_Digital_Image_Processing/lab/LAI_detection';
-path = strcat(path, '/bag_files/MC_20231130_150501.bag');
-
-im = brown_filter(path, 225, 125);
-imshow(im);
