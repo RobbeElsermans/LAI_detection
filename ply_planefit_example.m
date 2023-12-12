9cfa7cfbdf4af9ff1a261d7c89ea75b9cf2c0e2c
ahmadshakleya <shakleyaahmed@gmail.com>
2023-12-12 14:38:25 +0100

    File Structuur aangepast

--- ply_planefit_example.m
+++ ply_planefit_example.m
@@ -1,14 +1,19 @@
 clear
 close all
-%pointcloud = pcread('..\PLY files\LC_20231130_152650.ply');
-pointcloud = pcread(".\PLY files\LC_20231130_145946.ply")
+pointcloud = pcread('LAI_detection\LC_20231130_152650.ply');
 %clean up the pointcloud by setting the distance to an overestimation of the approximate distance to the wall
 % such that only actual outliers are removed
 [model,inlierIndices, outlierIndices] = pcfitplane(pointcloud,1);
 cleancloud = select(pointcloud, inlierIndices);
 pcshow(cleancloud)
+title("Cleaned dataset, outliners removed")
 
 estimatedLayerThickness = 0.01;
 [model,inlierIndices, outlierIndices] = pcfitplane(cleancloud,estimatedLayerThickness);
 firstlayer = select(cleancloud, inlierIndices);
 figure();pcshow(firstlayer)
+title("Leaves first layer")
+
+secondlayer = select(cleancloud, outlierIndices);
+figure();pcshow(secondlayer)
+title("Leaves second layer")
