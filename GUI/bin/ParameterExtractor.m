7322f0cd33ab9a064721b4efe06d325364e21c41
Robbe Elsermans <robels@outlook.com>
2023-12-19 22:57:29 +0100

    add importation of parameters in GUI

--- GUI/bin/ParameterExtractor.m
+++ GUI/bin/ParameterExtractor.m
@@ -27,7 +27,7 @@ classdef ParameterExtractor
             % Check if default file exists, otherwize write the default
             % values to it.
             if obj.checkExist() == 0
-                obj.WriteDataToFile(obj.default_value, obj.file_name)
+                obj.WriteDataToFile(obj.default_value)
             end
 
         end
@@ -40,7 +40,7 @@ classdef ParameterExtractor
                 % file has been deleted, reimport file with known values 
                 
                 if obj.checkExist() == 0
-                    obj.WriteDataToFile(obj.default_value, obj.file_name)
+                    obj.WriteDataToFile(obj.default_value)
                 end
 
                 data = obj.default_value;
@@ -48,18 +48,34 @@ classdef ParameterExtractor
             
         end
 
-        function error = WriteDataToFile(obj,data, file_name)
+        function data = ReadDataFromFile(obj)
+            % Read parameter data
+            try
+                data = readstruct(obj.file);
+            catch error_read
+                % file has been deleted, reimport file with known values 
+                
+                if obj.checkExist() == 0
+                    obj.WriteDataToFile(obj.default_value)
+                end
+
+                data = obj.default_value;
+            end
+            
+        end
+
+        function error = WriteDataToFile(obj,data)
             %Write data to a new file inside the file_path folder
             error = "";
 
-            new_file = strcat(obj.file_path, file_name);
+            %new_file = strcat(obj.file_path, file_name);
 
             try
                 if obj.checkExist() == 0 % if the file doesn't exist
-                    fclose(fopen(new_file, "w"));
+                    fclose(fopen(obj.file, "w"));
                 end
 
-                writestruct(data, new_file, PrettyPrint=true, FileType="json");
+                writestruct(data, obj.file, PrettyPrint=true, FileType="json");
             catch error_read
                 
                 error = error_read.message;
@@ -68,14 +84,26 @@ classdef ParameterExtractor
 
         function existed = checkExist(obj)
             if exist(obj.file, "file")
-                disp("file exists");
+                %disp("file exists");
                 existed = 1;
             else
                 existed = 0;
-                %obj.WriteDataToFile(obj.default_value, obj.file_name)
+                % obj.WriteDataToFile(obj.default_value, obj.file_name)
             end
 
         end
 
+        function setFilePath(obj, new_file_name)
+            % https://www.mathworks.com/help/matlab/ref/datetime.html
+            % prepareer de file name en naam
+            obj.file = new_file_name;
+            %obj.file = strcat(obj.file_path, obj.file_name);
+
+            % Check if default file exists, otherwize write the default
+            % values to it.
+            if obj.checkExist() == 0
+                obj.WriteDataToFile(obj.default_value);
+            end
+        end
     end
 end
