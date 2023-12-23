a62d8a0d441efcc74d9669b169cb04eec7bcc261
Robbe Elsermans <robels@outlook.com>
2023-12-23 21:38:25 +0100

    Done final adjustments

--- GUI/bin/CalcLai.m
+++ GUI/bin/CalcLai.m
@@ -1,24 +1,12 @@
-function [Leaf_area_index] = CalculateLAIfromPointCloud(countsInSlices, image_size, green_percent, hidden_leave_factor)
-% Assuming 'slicedPointClouds' contains the sliced point clouds
-
-% % Initialize an array to store the counts in each slice
-% countsInSlices = zeros(numel(slicedPointClouds), 1);
-% 
-% % Calculate the count in each slice
-% for i = 1:numel(slicedPointClouds)
-%     % Count the number of points in the slice
-%     close all
-%     %pcshow(slicedPointClouds{i})
-%     %pause
-%     countsInSlices(i) = numel(slicedPointClouds{i}.Location);
-% end
-
-% wall area in pixels;
-% image_size = 1280*720;
-
-% Divide each count by the depth x, y size
-result = countsInSlices / image_size; 
-
-Leaf_area_index = ((2/sum(result,"all"))*hidden_leave_factor*green_percent);
+function [Leaf_area_index] = CalcLai(countsInSlices, image_size, green_percent, hidden_leave_factor)
+    % Generate the Leaf Area Index by the amount of points in the depth
+    % image, the total amount of pixels in the image, the green percentage
+    % in the image and the hidden leave factor.
+    
+    
+    % Divide each count by the depth x, y size
+    result = countsInSlices / image_size; 
+    
+    Leaf_area_index = ((2/sum(result,"all"))*hidden_leave_factor*green_percent);
 
 end
--- GUI/bin/FindHiddenLeaveFactor.m
+++ GUI/bin/FindHiddenLeaveFactor.m
@@ -1,13 +1,12 @@
-function hidden_leave_factor = FindHiddenLeaveFactor(z_distance, hidden_leave_thresh)
+function hidden_leave_factor = FindHiddenLeaveFactor(z_distance, hidden_leave_thresh, param_1)
 % Determine the hidden leave value.
 % If the total z_distance is higher then 0.15, then we have probably hidden
 % leaves
 
-
-    % hidden_leave_thresh = 0.10;
+    % param_1 = 10;
     hidden_leave_factor = 1; % default, we have no hidden leaves
 
     if z_distance > hidden_leave_thresh
-        hidden_leave_factor = ((z_distance/hidden_leave_thresh)/10)+1; % +1 om ervoor te zorgen dat de factor erbij komt i.p.v. er af gaat
+        hidden_leave_factor = (z_distance/(hidden_leave_thresh*param_1))+1; % +1 om ervoor te zorgen dat de factor erbij komt i.p.v. er af gaat
     end
 end
